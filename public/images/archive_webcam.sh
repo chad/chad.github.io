5b4dee2e86f060bfce2c67c2cc99a1d91d0fbf6a
Chad Fowler <chad@chadfowler.com>
2013-09-14 14:00:40 -0700

    WTF

--- public/images/archive_webcam.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /usr/local/fowler/sites/chadfowler.com/html/images/
-cp webcam.jpg rubyconf/`ls -la webcam.jpg |awk '{print $6,$7,$8}'|sed 's/ //g'`.jpg
--- public/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-/* Author: Lynn Wallenstein */
-
-jQuery(function($) {
-
-  // Remove Preload so that transitions work after page loads
-  $("body").removeClass("preload");
-
-  // Handler for .ready() called.
-});
